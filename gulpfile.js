c74b0db7de8a6cf873a19b5ab6b5e32723d40642
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-04-15 14:38:57 +0700

    update gulp edit resume pdf and fix some words

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var path = require('path');
+var browserSync = require('browser-sync');
 
-gulp.task('mincss',function () {
-	gulp.src('src/**/*.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('/'));
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: "localhost:8081",
+        browser:"google chrome"
+    });
 });
 
-gulp.task('default',['mincss'],function(){
-	 gulp.watch("scss/**/*.css", ['mincss']);
+
+gulp.task('default', ['browser-sync'], function() {
+    gulp.watch(['./portfolio/css/*.css'], browserSync.reload);
+    gulp.watch(['./*.html'], browserSync.reload);
+		gulp.watch(['./*.css'], browserSync.reload);
+    gulp.watch(['./portfolio/*.html'], browserSync.reload);
 });
