0ed16b85f43e385f8c5990d84448c2372c5cc904
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-04-15 23:16:12 +0700

    redesign project section and fix tooltip

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var path = require('path');
 var browserSync = require('browser-sync');
+var scss = require("gulp-scss");
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -10,9 +11,17 @@ gulp.task('browser-sync', function() {
         browser:"google chrome"
     });
 });
+/*global require*/
+    "use strict";
+    gulp.task("scss", function () {
+        gulp.src(
+            "/portfolio/**/scss/*.scss"
+        ).pipe(scss(
+            {"bundleExec": true}
+        )).pipe(gulp.dest("/porftolio/css"));
+    });
 
-
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('default', ['browser-sync','scss'], function() {
     gulp.watch(['./portfolio/css/*.css'], browserSync.reload);
     gulp.watch(['./*.html'], browserSync.reload);
 		gulp.watch(['./*.css'], browserSync.reload);
